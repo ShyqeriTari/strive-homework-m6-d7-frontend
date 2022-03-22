17a2194f2191d693fc05527fff5cfdfefba0a238
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-22 16:47:48 +0100

    fetching from m6-d7 api

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+
+  const [blogs, setblogs] = useState([])
+
+  const apiUrl = "http://localhost:3001/blogs"
+
+  const fetchdata = async () => {
+    try {
+  
+      const response = await fetch(apiUrl)
+      const data = await response.json()
+  
+  if (response.ok){
+      setblogs(data)
+      console.log(data)
+      console.log(blogs)
+  }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect (()=> {fetchdata()}, [])
+
+
   return (
     <Row>
-      {posts.map((post) => (
+      {blogs.map((blog) => (
         <Col
           md={4}
           style={{
             marginBottom: 50,
           }}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem key={blog.title} {...blog} />
         </Col>
       ))}
     </Row>
